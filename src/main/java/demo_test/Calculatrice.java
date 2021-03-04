31cffe1d051e304cd21f82824f71373f728d2e9c
Remi-Assidi <remi.assidi@epsi.fr>
2021-03-04 16:40:35 +0100

    Add

--- src/main/java/demo_test/Calculatrice.java
+++ src/main/java/demo_test/Calculatrice.java
@@ -2,23 +2,23 @@ package demo_test;
 
 public class Calculatrice {
 
-		public int add(int a, int b) {
-			return a + b;
-		}
-		
-		public int sub(int a, int b) {
-			return a - b;
-		}
-		
-		public int mul(int a, int b) {
-			return a * b;
-		}
-		
-		public int div(int a, int b) {
-			return a / b;
-		}
-		
-		public boolean div(int a) {
-			return true;
-		}
+    public int add(int a, int b) {
+        return a + b;
+    }
+    
+    public int sub(int a, int b) {
+        return a - b;
+    }
+    
+    public int mul(int a, int b) {
+        return a * b;
+    }
+    
+    public int div(int a, int b) {
+        return a / b;
+    }
+    
+    public boolean isPair(int a) {
+        return ((a % 2) == 0);
+    };
 }
--- src/test/java/demo_test/Calculatrice_Test.java
+++ src/test/java/demo_test/Calculatrice_Test.java
@@ -10,52 +10,58 @@ import org.junit.Assert;
 
 public class Calculatrice_Test {
 
-	    @Before
-	    public void setUp() throws Exception {
-	    }
+	@Before
+    public void setUp() throws Exception {
+    }
 
-	    @After
-	    public void tearDown() throws Exception {
-	    }
+    @After
+    public void tearDown() throws Exception {
+    }
 
-	    @Test
-	    public void testAdd() {
-	        int a = 2;
-	        int b = 3;
-	        Calculatrice calc = new Calculatrice();
-	        int actual = calc.add(a, b);
-	        
-	        Assert.assertEquals("Addition fail",5, actual);
-	    }
+    @Test
+    public void testAdd() {
+        int a = 2;
+        int b = 3;
+        Calculatrice calc = new Calculatrice();
+        int actual = calc.add(a, b);
+        
+        Assert.assertEquals(5, actual);
+    }
 
-	    @Test
-	    public void testSub() {
-	    int a = 4;
-	    int b = 3;
-	    Calculatrice calc = new Calculatrice();
-	    int actual = calc.sub(a, b);
-	    
-	    Assert.assertEquals("Substraction fail",1, actual);
-	    }
-	    
-	    @Test
-	    public void testMul() {
-	    int a = 4;
-	    int b = 3;
-	    Calculatrice calc = new Calculatrice();
-	    int actual = calc.mul(a, b);
-	    
-	    Assert.assertEquals("Multiplication fail",12, actual);
-	    }
-	    
-	    @Test
-	    public void testDiv() {
-	    int a = 6;
-	    int b = 3;
-	    Calculatrice calc = new Calculatrice();
-	    int actual = calc.sub(a, b);
-	    
-	    Assert.assertEquals("Division fail",2, actual);
-	    }
+    @Test
+    public void testSub() {
+        int a = 7;
+        int b = 3;
+        Calculatrice calc = new Calculatrice();
+        int actual = calc.sub(a, b);
+        
+        Assert.assertEquals(4, actual);    }
+
+    @Test
+    public void testMul() {
+        int a = 2;
+        int b = 3;
+        Calculatrice calc = new Calculatrice();
+        int actual = calc.mul(a, b);
+        
+        Assert.assertEquals(6, actual);
+    }
+
+    @Test
+    public void testDiv() {
+        int a = 12;
+        int b = 3;
+        Calculatrice calc = new Calculatrice();
+        int actual = calc.div(a, b);
+        
+        Assert.assertEquals(4, actual);    }
+    
+    @Test
+    public void testPair() {
+        int a = 12;
+        Calculatrice calc = new Calculatrice();
+        boolean actual = calc.isPair(a);
+        
+        Assert.assertTrue(actual);    }
 
 }
